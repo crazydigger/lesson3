eccc29b015a78ef2614e5b5f5bb2037e95b28231
borovirp <40pobGsb>
2021-12-15 11:18:22 +0300

    new update

--- dz3-light.py
+++ dz3-light.py
@@ -1,7 +1,14 @@
 # new list
 l = [1, 2, 3, 4]
-print("1.l.append(5),2.l.remove(1)l, 3.len(l,4.l.sort(,reversed=True),5.l.reverse()")
+print("методы списков:\n1.l.append(5),2.l.remove(1)l, 3.len(l,4.l.sort(,reversed=True),5.l.reverse()")
 
 # new dict
 d = {1: 'First', 2: 'second', 3: 'third', }
-print(d)
+print("методы словарей:\n1.l.update({4:'forth'}),2.l.popitem(1:firt)l, 3.d.len(),5.sorted(d.values(),reverse=true")
+
+#новое множество
+s=set((1,2,3))
+print("методы множеств:\n1.s.add((4)),2.s.remove(1), 3.s.len(),5.sorted(s,reverse=true")
+#новая строка
+str='hello Python'
+print("методы строк:\natr.s.upper((4)),2.str.lower(1), 3.str.len(),5.str.find(\"hello\")
