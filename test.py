7659dccd917c50c3f7071af6efe51a377b4c4860
borovirp <40pobGsb>
2021-12-16 17:28:27 +0300

    new update

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
-numbers = '2,3,4,5,5,6,5,3,9'
-print(numbers)
-l = list(numbers.split(','))
-l =sorted(l)
-print(set(l))
+# словари
+name = 'max'
+age = 20
+has_car = True
+
+persons = (name, age, has_car)
+
+friend_inverse = (age, has_car, name)
+
+# Словарь, объявление
+persons = {
+    'name': 'а.с.пушкин',
+    'bd': '9.091928',
+    'has_car':
+}
+
+print(type(persons)
+
+famous_persons = {name:'а.с.пушкин':'9.091928',bd
+                  'л.н.толстой:'28.081828',
+                'фмдостоевский':'1830'}
+print(famous_persons)
+
--- victory.py
+++ victory.py
@@ -1,4 +1,16 @@
-famous_persons = {'а.с.пушкин':'9.091928',
-                  'л.н.толстой:'28.081828',
-                'фмдостоевский':'1830'}
-print(famous_persons)
+from random import randint
+import random
+
+names = 'а.с.пушкин,л.н.толстой,фмдостоевский,мюлермонтов,апчехов'.split(',')
+dates = '9.091928,28.081828,11111921,27061941,29011960'.split(',')
+print(names)
+print(dates)
+famous_persons={}
+for i in range(len(names)):
+#famous_persons.fromkeys(names)
+    famous_persons={names[i]:dates[i]}
+
+i = random.randint(0,len(names))
+print('!!!!!!!!!!!!!!!!!random:',i)
+
+#dates=(
