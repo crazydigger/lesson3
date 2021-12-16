d0a6660509fddc168dc0fda0015251252c6cd0af
borovirp <40pobGsb>
2021-12-16 19:28:45 +0300

    new update

--- test.py
+++ test.py
@@ -1,23 +1,3 @@
-# словари
-name = 'max'
-age = 20
-has_car = True
-
-persons = (name, age, has_car)
-
-friend_inverse = (age, has_car, name)
-
-# Словарь, объявление
-persons = {
-    'name': 'а.с.пушкин',
-    'bd': '9.091928',
-    'has_car':
-}
-
-print(type(persons)
-
-famous_persons = {name:'а.с.пушкин':'9.091928',bd
-                  'л.н.толстой:'28.081828',
-                'фмдостоевский':'1830'}
-print(famous_persons)
-
+names = "а.с.пушкин,л.н.толстой,фмдостоевский,мюлермонтов,апчехов".split(',')
+dates = '9.091928,28.081828,11111921,27061941,29011960'.split(',')
+print('right answer!!!!!!!!!!!!!!!!', famous_personsnames[0])
--- victory.py
+++ victory.py
@@ -1,16 +1,33 @@
 from random import randint
 import random
 
-names = 'а.с.пушкин,л.н.толстой,фмдостоевский,мюлермонтов,апчехов'.split(',')
+names = "а.с.пушкин,л.н.толстой,фмдостоевский,мюлермонтов,апчехов".split(',')
 dates = '9.091928,28.081828,11111921,27061941,29011960'.split(',')
 print(names)
 print(dates)
 famous_persons={}
 for i in range(len(names)):
-#famous_persons.fromkeys(names)
+    famous_persons=famous_persons.fromkeys(names)
+    print(names[i],dates[i])
     famous_persons={names[i]:dates[i]}
+    #famous_persons(names[i],dates[i])
+print(famous_persons)
 
-i = random.randint(0,len(names))
-print('!!!!!!!!!!!!!!!!!random:',i)
 
-#dates=(
+
+count_right=0
+for question in names:
+    print('name:::',famous_persons[question])
+    try:
+        print('right answer!!!!!!!!!!!!!!!!', famous_persons[names[0]])
+    #except:
+        continue
+
+
+        d_answ=input('введите дату рождения'+question+ 'в формате ddmmyyyy:')
+        if(d_answ==famous_persons[question]):
+
+            print('right answer!!!!!!!!!!!!!!!!',famous_persons[question])
+    except:
+        pass
+        count_right+=1
